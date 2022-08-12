dda91699bec00696ea087589cbc8ed028ce9aa9a
zzcipod <jonzzheng@gmail.com>
2022-08-12 00:33:55 -0700

    data

--- utils/seed.js
+++ utils/seed.js
@@ -1,6 +1,6 @@
 const connection = require('../config/connection');
 const { Course, Student } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+// const { getRandomName, getRandomAssignments } = require('./data');
 
 connection.on('error', (err) => err);
 
@@ -14,38 +14,38 @@ connection.once('open', async () => {
   await Student.deleteMany({});
 
   // Create empty array to hold the students
-  const students = [];
+  // const students = [];
 
   // Loop 20 times -- add students to the students array
-  for (let i = 0; i < 20; i++) {
+  // for (let i = 0; i < 20; i++) {
     // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
+    // const assignments = getRandomAssignments(20);
 
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+    // const fullName = getRandomName();
+    // const first = fullName.split(' ')[0];
+    // const last = fullName.split(' ')[1];
+    // const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
 
-    students.push({
-      first,
-      last,
-      github,
-      assignments,
-    });
-  }
+  //   students.push({
+  //     first,
+  //     last,
+  //     github,
+  //     assignments,
+  //   });
+  // }
 
   // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
+  // await Student.collection.insertMany(students);
 
   // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...students],
-  });
+  // await Course.collection.insertOne({
+  //   courseName: 'UCLA',
+  //   inPerson: false,
+  //   students: [...students],
+  // });
 
   // Log out the seed data to indicate what should appear in the database
-  console.table(students);
+  // console.table(students);
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
