d7df6b8e8a565528e7022b0e138e681c03c599b6
zzcipod <jonzzheng@gmail.com>
2022-08-17 12:01:52 -0700

    changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,62 +4,65 @@ module.exports = {
   // Get all users
   getUsers(req, res) {
     User.find()
-      .populate("thoughts")
-      .populate("friends")
-        .then((users) => res.json(users))
-        .catch((err) => res.status(500).json(err));
+    .populate("thoughts")
+    .populate("friends")
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+      
   },
-  // Get a single student
+  // Get a users
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .populate("thoughts")
       .populate("friends")
       .select('-__v')
-      // .lean()
-      .then(async (user) =>
+      .then((user) =>
         !user
-          ? res.status(404).json({ message: 'No user with that ID' })
+          ? res.status(404).json({ message: 'Cant find that ID' })
           : res.json(user)
       )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
       .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
       });
   },
-  // create a new user
-  createUser(req, res) {
-    User.create(req.body)
-      .then((user) => res.json(user))
-      .catch((err) => res.status(500).json(err));
-  },
-  // Delete a student and remove them from the course
+  // Delete a user
   deleteUser(req, res) {
-    User.findOneAndRemove({ _id: req.params.userId })
+    User.findOneAndDelete({ _id: req.params.userId })
       .then((user) =>
         !user
-          ? res.status(404).json({ message: 'No such user exists' })
-          : Thought.deleteMany(
-              {_id:{$in:user.thoughts}}
-            )
+          ? res.status(404).json({ message: 'Cant find that ID' })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
       )
-      .then(() =>
+      .then(() => res.json({ message: 'user deleted' }))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Update a user
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
         !user
-          ? res.status(404).json({
-              message: 'No user found',
-            })
+          ? res.status(404).json({ message: 'Cant find that ID' })
           : res.json(user)
       )
-      .catch((err) => 
-        // console.log(err);
-        res.status(500).json(err)
-      );
+      .catch((err) => res.status(500).json(err));
   },
 
-  // Add an assignment to a student
+  // Add a friend
   addFriend(req, res) {
-    console.log('You are adding a friend');
+    console.log('Added friend');
     console.log(req.body);
-    Student.findOneAndUpdate(
+    User.findOneAndUpdate(
       { _id: req.params.userId },
       { $addToSet: { friends: req.params.friendId } },
       { runValidators: true, new: true }
@@ -68,12 +71,12 @@ module.exports = {
         !user
           ? res
               .status(404)
-              .json({ message: 'No user found with that ID :(' })
+              .json({ message: 'Cant find that ID' })
           : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
-  // Remove assignment from a student
+  // Remove friend from user
   removeFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -84,9 +87,10 @@ module.exports = {
         !user
           ? res
               .status(404)
-              .json({ message: 'No student found with that ID :(' })
+              .json({ message: 'Cant find that ID' })
           : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
+  
 };
